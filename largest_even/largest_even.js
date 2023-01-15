c645aa636dc37084da6e4df7c392e67c29f38574
sidx255 <siddharth190820@gmail.com>
2023-01-15 23:30:32 +0530

    feat: find and test largest even integer in array

---
+++ largest_even/largest_even.js
@@ -0,0 +1,21 @@
+// Using normal function
+function largestEven(nums) {
+    let evenNum = -1
+    console.log(evenNum)
+    nums.forEach(val => {
+        evenNum = val % 2 === 0 && evenNum < val ? val : evenNum
+    })
+    return evenNum
+}
+
+// Using arrow function
+const largestEvenUsingArrowFunction = (nums) => {
+    let evenNum = -1
+    console.log(evenNum)
+    nums.forEach(val => {
+        evenNum = val % 2 === 0 && evenNum < val ? val : evenNum
+    })
+    return evenNum
+}
+
+module.exports = { largestEven, largestEvenUsingArrowFunction };
---
+++ largest_even/largest_even.test.js
@@ -0,0 +1,20 @@
+const { largestEven } = require('./largest_even');
+const { largestEvenUsingArrowFunction} = require('./largest_even');
+
+describe('finding the largest even number', () => {
+    test('[1, 2, 3, 4, 6] should give "6" ', () => {
+        expect(largestEven([1, 2, 3, 4, 6])).toBe(6);
+    });
+    test('[1, 3, 5, 7] should give "-1" ', () => {
+        expect(largestEven([1, 3, 5, 7])).toBe(-1);
+    })
+});
+
+describe('finding the largest even number using arrow function', () => {
+    test('[-11, 2, 3, 4, 8] should give "8" ', () => {
+        expect(largestEvenUsingArrowFunction([-11, 2, 3, 4, 8])).toBe(8);
+    });
+    test('[1, 3, 5, 7] should give "-1" ', () => {
+        expect(largestEvenUsingArrowFunction([1, 3, 5, 7])).toBe(-1);
+    })
+});
