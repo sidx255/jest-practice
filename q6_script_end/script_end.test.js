02d5818cf69898af95775563c93057ed475fc7ca
sidx255 <siddharth190820@gmail.com>
2023-01-16 01:36:08 +0530

    feat: solve and test if strings ends with Script

---
+++ q6_script_end/script_end.test.js
@@ -0,0 +1,20 @@
+
+const { checkEnd, checkEndArrow } = require('./script_end')
+
+describe('checking the ending of string', () => {
+    test('JavavavavavScript should give true', () => {
+        expect(checkEnd("JavavavavavScript")).toBe(true);
+    });
+    test('Script should give true', () => {
+        expect(checkEnd("Script")).toBe(true);
+    })
+});
+
+describe('checking the ending of string using arrow function', () => {
+    test('javascript should give false', () => {
+        expect(checkEndArrow("javascript")).toBe(false);
+    });
+    test('java.Script should give true', () => {
+        expect(checkEndArrow("java.Script")).toBe(true);
+    })
+});
