6a52804a6a53d39a6ba18e4218e29033c972da87
sidx255 <siddharth190820@gmail.com>
2023-01-16 01:22:04 +0530

    feat: solve and test finding leap year

---
+++ q5_leap_year/leap_year.js
@@ -0,0 +1,13 @@
+
+function leapYear(year) {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+const leapYearArrow = year => {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+//console.log(leapYear(2000));
+//console.log(leapYearArrow(1996))
+
+module.exports = { leapYear, leapYearArrow };
---
+++ q5_leap_year/leap_year.test.js
@@ -0,0 +1,20 @@
+
+const {leapYear, leapYearArrow} = require('./leap_year')
+
+describe('finding leap year', () => {
+    test('2004 should give true', () => {
+        expect(leapYear(2004)).toBe(true);
+    });
+    test('2001 should give false', () => {
+        expect(leapYear(2001)).toBe(false);
+    })
+});
+
+describe('finding leap year using arrow function', () => {
+    test('1996 should give true', () => {
+        expect(leapYearArrow(1996)).toBe(true);
+    });
+    test('1911 should give false', () => {
+        expect(leapYearArrow(1991)).toBe(false);
+    })
+});
