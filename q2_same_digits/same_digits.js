c9e514ea70d78d0be52448e8bf4b3c61eecc7093
sidx255 <siddharth190820@gmail.com>
2023-01-16 00:10:43 +0530

    feat: solve and test same digits in a number

---
+++ q2_same_digits/same_digits.js
@@ -0,0 +1,19 @@
+//var num = 0;
+//var num2 = -12;
+
+// using regular function
+function areSame(num) {
+    var arr = String(num).split('');
+    return arr.every(function (el) { return el === arr[0]; });
+}
+
+// using arrow function
+const areSameArrow = num => {
+    var arr = String(num).split('');
+    return arr.every(function (el) { return el === arr[0]; });
+}
+
+//console.log(areSameArrow(num));
+//console.log(areSameArrow(num2));
+
+module.exports = { areSame, areSameArrow };
