27ff906e2650fe7e80c110a69aa14cd301ecb217
sidx255 <siddharth190820@gmail.com>
2023-01-16 01:56:03 +0530

    feat: solve and test concatination of strings

---
+++ q7_concat_strings/concat_strings.js
@@ -0,0 +1,15 @@
+
+function concatStr(arr){
+    if(arr.length==2)
+    return arr[0].slice(1, arr[0].length)+arr[1];
+}
+
+const concatStrArrow = arr => {
+    if(arr.length==2)
+    return arr[0].slice(1, arr[0].length)+arr[1];
+}
+
+//arr = ["Siddharth", "Sharma"];
+//console.log(concatStr(arr));
+
+module.exports = {concatStr, concatStrArrow};
---
+++ q7_concat_strings/concat_strings.test.js
@@ -0,0 +1,20 @@
+
+const { concatStr, concatStrArrow } = require('./concat_strings')
+
+describe('concatinating strings', () => {
+    test('Java and Script should give avaScript', () => {
+        expect(concatStr(["Java", "Script"])).toBe("avaScript");
+    });
+    test('Sid and Sharma should give idSharma', () => {
+        expect(concatStr(["Sid", "Sharma"])).toBe("idSharma");
+    })
+});
+
+describe('concatinating strings using arrow function', () => {
+    test('Concat and Strings should give oncatStrings', () => {
+        expect(concatStrArrow(["Concat", "Strings"])).toBe("oncatStrings");
+    });
+    test('Code and Academy should give odeAcademy', () => {
+        expect(concatStrArrow(["Code", "Academy"])).toBe("odeAcademy");
+    })
+});
