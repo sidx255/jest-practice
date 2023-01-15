90e0a6a534ddd281e6e8a69b2e842912e7a4ac1a
sidx255 <siddharth190820@gmail.com>
2023-01-16 00:32:51 +0530

    feat: solve and test changing case of each letter in a string

---
+++ q3_change_caps/change_cap.js
@@ -0,0 +1,26 @@
+
+//str1 = "sIddhartH";
+//str2 = "SHARma";
+
+// using normal function
+function changeCap(str) {
+    var str1 = "";
+    for (var i = 0; i < str.length; i++) {
+        if (/[A-Z]/.test(str[i])) str1 += str[i].toLowerCase();
+        else str1 += str[i].toUpperCase();
+    }
+    return str1;
+}
+
+// using arrow function
+const changeCapArrow = str => {
+    var str1 = "";
+    for (var i = 0; i < str.length; i++) {
+        if (/[A-Z]/.test(str[i])) str1 += str[i].toLowerCase();
+        else str1 += str[i].toUpperCase();
+    }
+    return str1;
+}
+
+//console.log(changeCap(str2));
+//console.log(changeCapArrow(str1));
